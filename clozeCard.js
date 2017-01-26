52400e4284fdcf09d7267d965dca595195912f15
Chris Sayles <chrissayles@Chriss-MacBook-Pro-2.local>
2017-01-26 13:49:41 -0600

    Working on cloze question bug when incorrect answer is submitted

--- clozeCard.js
+++ clozeCard.js
@@ -1,25 +1,26 @@
 console.log("cloze file linked");
+
 exports.clozeQuestions = [{
-	front: "What year did the Jackson 5 leave Motown to sign with Epic Records?",
-    back: "1975"
+	text: "What year did the Jackson 5 leave Motown to sign with Epic Records?",
+    cloze: "1975"
 },
 {
-	front: "It was at the 'Motown 25: Yesterday, Today, Forever' special that this artist first publicly performed his moonwalk on national television. Who was this great artist?",
-	back: "Michael Jackson"
+	text: "It was at the 'Motown 25: Yesterday, Today, Forever' special that this artist first publicly performed his moonwalk on national television. Who was this great artist?",
+	cloze: "Michael Jackson"
 },
 {
-	front:"Which Motown artist had a hit with, 'What Becomes Of The Brokenhearted'?",
-	back: "Jimmy Ruffin"
+	text:"Which Motown artist had a hit with, 'What Becomes Of The Brokenhearted'?",
+	cloze: "Jimmy Ruffin"
 },
 {
-	front:"In which US city was Motown records originally based?",
-	back: "Detroit"
+	text:"In which US city was Motown records originally based?",
+	cloze: "Detroit"
 },
 {
-	front: "You and I must make a pact, we must bring salvation back. Where there is love, I'll be there...Was sung by which group?",
-	back: "Jackson 5"
+	text: "You and I must make a pact, we must bring salvation back. Where there is love, I'll be there...Was sung by which group?",
+	cloze: "Jackson 5"
 },
 {
-	front: "Who founded Motown Records Motown Records?",
-	back: "Berry Gordy Jr."
+	text: "Who founded Motown Records Motown Records?",
+	cloze: "Berry Gordy Jr."
 }];
--- flashcard.js
+++ flashcard.js
@@ -1,13 +1,11 @@
- 
-
 //Required packages
 var inquirer = require("inquirer");
 var fs = require("fs");
-//Required Variables if I choose to go the route of having seperate files for basic and cloze
+//Required Variables for External Files
 var basicCard = require("./basicCard.js");
-// var clozeCard = require("./clozeCard.js");
-// var questions = require("./questions.json");
-// var count = 0;
+var clozeCard = require("./clozeCard.js");
+
+
 
 //BASIC CARD CONSTRUCTOR
  function BasicCard(front,back){
@@ -15,80 +13,136 @@ var basicCard = require("./basicCard.js");
   this.front = front;
   this.back = back;
 };
-
+function ClozeCard(text,cloze){
+  this.text = text;
+  this.cloze = cloze;
+  this.fullText = function(){
+    return this.text + this.cloze;
+  }
+};
 var count = 0;
-//Function which prompts the user to select what type of flashcard to select
-var askQuestionType = function(){
-//Inquirer Package gives drop down list of Flashcard choices
-inquirer.prompt([
+var score = 0;
+
+
+
+
+var askQuestion = function() {
+
+if (count < basicCard.basicQuestions.length) {
+
+    inquirer.prompt([
+      {
+        name: "input",
+        message: basicCard.basicQuestions[count].front
+      }
+    ]).then(function(answers) {
+      if (answers.input === basicCard.basicQuestions[count].back) {
+        console.log("You're Groovin!");
+        score++;
+      } else {
+        console.log("Who's Bad?.  The correct answer is " + basicCard.basicQuestions[count].back);
+      }
+      
+      count++;
+      askQuestion();
+    });
+  } else {
+
+      var gameOver = true;
+      count = 0;
+      if (gameOver === true)
+        {
+          inquirer.prompt([
+            {
+              type: "list",
+              name: "game",
+              message: "Was it just my imagination? Your Score was " + score + " answers correct\n  Would you like to try again?",
+              choices: ["Yes", "No"]
+            }
+            ]).then(function (answer) {
+            if(answer.game === "Yes") {
+              startgame();
+              endGame = false;
+            } else {
+              console.log("I know you want to leave me but i refuse to let you go!");
+            }
+
+        });
+
+    }
+
+  }
+}
+
+var askCloze = function() {
+if (count < clozeCard.clozeQuestions.length) {
+  inquirer.prompt([
+      {
+        name: "input",
+        message: clozeCard.clozeQuestions[count].text + "____________"
+      }
+     ]).then(function(answers) {
+      if (answers.input === clozeCard.clozeQuestions[count].cloze) {
+        console.log("You're Groovin!");
+        score++;
+      } else {
+        console.log("Who's Bad?.  The correct answer is " + clozeCard.clozeQuestions[count].fullText());
+      }
+      count++;
+      askCloze();
+    });
+     } else {
+
+      var gameOver = true;
+      count = 0;
+      if (gameOver === true)
+        {
+          inquirer.prompt([
+             {
+              type: "list",
+              name: "game",
+              message: "Was it just my imagination? Your Score was " + score + " answers correct\n  Would you like to try again?",
+              choices: ["Yes", "No"]
+            }
+            ]).then(function (answer) {
+            if(answer.game === true) {
+              startgame();
+              endGame = false;
+            } else {
+              console.log("I know you want to leave me but i refuse to let you go!");
+            }
+
+        });
+
+    }
+
+  }
+}
+
+  
+
+
+
+
+
+var startgame = function () {
+  count=0;
+  score=0;
+  inquirer.prompt([
   {
     type: "list",
     message: "Choose from the Following:",
     choices: ["Basic", "Cloze"],
     name: "chooseType"
   }
-  ]).then(function(userInput) {
-    //If user selects  Basic the askBasicQuestion function will trigger
-  	if(userInput.chooseType === "Basic"){
-  		console.log("choosing Basic");
-  		askBasicQuestion();
-    //If user selects Cloze the askClozeQuestion function will trigger
-  	}else if(userInput.chooseType === "Cloze"){
-  		console.log("choosing Cloze")
-    }
-   });
- }
-
- askQuestionType();
-
-
-
-function askBasicQuestion() {
-  //new card will populate from the basicCard.js with the question and answer
-	var newCard = new BasicCard(basicCard.basicQuestions[count].front,
-		basicCard.basicQuestions[count].back);
-  //text will display the question and require the user to type the answer
-	inquirer.prompt([
-	{
-		type:"input",
-		message: newCard.front,
-		name: "userAnswer"
-	}
-  //if the user types in the exact answer the console will display "You got it right" and the count will increment up
-	]).then(function(user){
-		var correctChoice = newCard.back;
-		if(user.userAnswer === correctChoice){
-			console.log("You got it right!");
-			count++;
-			askBasicQuestion();
-      //If user types in the incorrect answer the console will log (You got it wrong, the correct answer is__)
-		}else{
-			console.log("You got it wrong, the correct answer is: " + newCard.back);
-			count++;
-      startOverBasic();
-			askBasicQuestion();
-		}
-	});
-}
-
-function startOverBasic(){
-     inquirer.prompt([
-  {
-    name: "start",
-    message: "Which flash cards do you want?  Type Basic or Cloze to select."
-
-  }
     ]).then(function (answers) {
-    if (answers.start === "Basic") {
+    if (answers.chooseType === "Basic") {
       askQuestion();
     }
     else {
-      console.log("Clozure Function")
+      askCloze();
     }
 });
 }
 
-startOverBasic();
-
-
-
+startgame(); 
